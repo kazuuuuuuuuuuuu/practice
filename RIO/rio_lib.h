2e7b4fdba385ec12c3090dc8ffb87cf6cabb46af
kazuuuuuuuuuuuu <li-278139@hotmail.com>
2024-01-03 15:46:25 +0000

    Update rio_lib.h
    update

--- RIO/rio_lib.h
+++ RIO/rio_lib.h
@@ -11,7 +11,7 @@
 ssize_t rio_readn(int fd, void *usrbuf, size_t n);
 ssize_t rio_writen(int fd, void *usrbuf, size_t n);
 
-// read and write with application buffer
+// read with application buffer
 struct rio_t
 {
 	int rio_fd;
