37137beef6bbcec4cd8e4963f2c67ac91a7463f7
kazuuuuuuuuuuuu <li-278139@hotmail.com>
2024-03-15 16:46:48 +0000

    friday

--- udp/server2.cpp
+++ udp/server2.cpp
@@ -7,6 +7,9 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+// 使用 非阻塞io using unblocked io
 
 #define BUFF_SIZE 1024
 
@@ -32,8 +35,12 @@ int main(int argc, char const *argv[])
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_len = sizeof(struct sockaddr_in);
 
-	// 1 create a socket
+	// 1 create a socket -> udp and unblocked io
+	// server_sockfd = socket(AF_INET, SOCK_DGRAM|SOCK_NONBLOCK, 0);
 	server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	// set unblocked io
+	fcntl(server_sockfd, F_SETFL, fcntl(server_sockfd, F_GETFL, 0) | O_NONBLOCK);
 
 	// 2 set the server address
 	server_addr.sin_family = AF_INET;
@@ -55,6 +62,12 @@ int main(int argc, char const *argv[])
 		recv_len = recvfrom(server_sockfd, buff, sizeof(buff), 0, (struct sockaddr *)&client_addr, &client_addr_len);
 		if(recv_len<0)
 		{
+			// data is not ready -> retry
+			if(errno==EAGAIN||errno==EWOULDBLOCK)
+			{
+				sleep(2);
+				continue;
+			}
 			perror("recvfrom");
 			exit(1);
 		}
